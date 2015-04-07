b2472dfeb7afa8d749e578b708097fc6f3fe9b6d
Francesco Pezzulli <ipezzu@gmail.com>
2015-04-07 17:18:14 +0200

    versione 1.0

---
+++ src/control/Parole.java
@@ -0,0 +1,59 @@
+package control;
+
+// @author Francesco
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+
+
+public class Parole {
+    
+    int uniche, ripetute, totale;
+    HashSet h;
+    String[] book;
+    
+    public Parole() {
+        uniche = 0;
+        ripetute = 0;
+        totale = 0;
+        h = new HashSet();
+    }
+    
+    public void read(String filename) {
+        try {
+            FileInputStream f = new FileInputStream(filename);
+            InputStreamReader in = new InputStreamReader(f);
+            BufferedReader b = new BufferedReader(in);
+            while (b.readLine()!=null) {
+                String s = b.readLine();
+                book = s.split(" |,|\\.|;|:|'|-|!|\\?");
+                for (int i = 0; i < book.length; i++) {
+                    if (h.contains(book[i])) {
+                        ripetute++;
+                        totale++;
+                    }
+                    else {
+                        uniche++;
+                        totale++;
+                    }
+                    h.add(book[i]);
+                }
+            }
+            
+            System.out.println("Totale parole: " + totale);
+            System.out.println("Paole uniche: " + uniche);
+            System.out.println("Parole ripetute: " + ripetute);
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File non trovato!");
+        } catch (IOException ex) {
+            System.out.println("Errore nell'apertura del file");
+        }
+    }
+    
+}
---
+++ src/paroleset/ParoleSet.java
@@ -0,0 +1,14 @@
+package paroleset;
+
+// @author Francesco
+
+import control.Parole;
+
+
+public class ParoleSet {
+    public static void main(String[] args) {
+        Parole p = new Parole();
+        p.read("data/miseriaenobilta.txt");
+    }
+    
+}
