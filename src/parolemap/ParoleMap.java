f641efbfbda79e4a721333ffc0cc1f900f1d55ba
Francesco Pezzulli <ipezzu@gmail.com>
2015-04-07 17:17:02 +0200

    versione 1.0

---
+++ src/parolemap/ParoleMap.java
@@ -0,0 +1,14 @@
+package parolemap;
+
+// @author Francesco
+
+import control.Parole;
+
+
+public class ParoleMap {
+    public static void main(String[] args) {
+        Parole p = new Parole();
+        p.read("data/miseriaenobilta.txt");
+    }
+    
+}
